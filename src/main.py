2b4352e65eefb210d0fd80bca87b03a0756b1603
dalet <victordalet@protonmail.com>
2023-11-08 23:33:25 +0100

    fix(add picture): send picture with no error

--- src/main.py
+++ src/main.py
@@ -87,8 +87,8 @@ class Main:
                         self.api.add_data_city(self.detector.get_nb_beach(),
                                                self.detector.get_nb_sea(),
                                                self.detector.get_visibility()),
-                # self.api.add_picture_alert_or_moment(
-                #    FOLDER_PICTURE + self.city + '.png')
+                self.api.add_picture_alert_or_moment(
+                    FOLDER_PICTURE + self.city + '.png')
 
 
 if __name__ == '__main__':
